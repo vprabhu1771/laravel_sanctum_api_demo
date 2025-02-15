a270b2789ce56be092aea62499b7f65edb5bc97a
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-15 09:53:45 +0530

    login and logout using only api routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Login
+use App\Models\User;
+// use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+ 
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+// Logout
+Route::middleware('auth:sanctum')->get('/user/revoke', function (Request $request) {
+    
+    $user = $request->user();
+
+    $user->tokens()->delete();
+
+    return 'tokens are deleted';
+
+});
